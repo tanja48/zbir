45dc07605343c12542724ca28df6847967886c6c
tika <tika@tika-PC>
2018-11-13 12:23:40 +0100

    Commit

---
+++ PrvihDeset/src/SumaPrvihDeset.java
@@ -0,0 +1,14 @@
+
+public class SumaPrvihDeset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int suma=0;
+		for(int tekuci=1; tekuci<= 10; tekuci ++) {
+			suma += tekuci;
+					}
+System.out.println("Suma = "+ suma);
+	}
+
+}
